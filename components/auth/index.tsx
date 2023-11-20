173dafba1311648252ceb1cad7729cc8a2752ccc
Mirabbosbek9778 <jwicj969@gmail.com>
2023-11-21 00:19:36 +0500

    sign in and sign up Schema alidation abd onclick signin/signup

--- components/auth/index.tsx
+++ components/auth/index.tsx
@@ -1,12 +1,30 @@
+"use client";
+
 import Image from "next/image";
-import React from "react";
+import React, { useCallback } from "react";
 import Button from "../ui/button";
 import { FcGoogle } from "react-icons/fc";
 import { AiFillGithub } from "react-icons/ai";
+import useRegisterModal from "@/hooks/useRegisterModal";
+import RegisterModal from "../modals/registerModal";
+import useLoginModal from "@/hooks/useLoginModal";
+import LoginModal from "../modals/loginModal";
 
 const Auth = () => {
+  const registerModal = useRegisterModal();
+  const loginModal = useLoginModal();
+
+  const onOpenRegisterModal = useCallback(() => {
+    registerModal.onOpen();
+  }, [registerModal]);
+
+  const onOpenLoginModal = useCallback(() => {
+    loginModal.onOpen();
+  }, [loginModal]);
   return (
     <>
+      <RegisterModal />
+      <LoginModal />
       <div className="grid grid-cols-1 md:grid-cols-2 gap-10 items-center h-screen">
         <Image
           src={"/images/x.svg"}
@@ -45,14 +63,18 @@ const Auth = () => {
               />
               <div className="flex items-center justify-center">
                 <div className="h-px bg-gray-700 w-1/2" />
-                <p className="mx-4">OR</p>
+                <p className="mx-4">or</p>
                 <div className="h-px bg-gray-700 w-1/2" />
               </div>
-              <Button label={"Create Account"} fullWidth />
+              <Button
+                label={"Create Account"}
+                fullWidth
+                onClick={onOpenRegisterModal}
+              />
               <div className="text-[10px] text-gray-400">
                 By signing up, you agree to the
                 <span className="text-sky-500">Terms of Service</span> and
-                <span className="text-sky-500">Privace Policy</span>, including
+                <span className="text-sky-500">Privacy Policy</span>, including
                 <span className="text-sky-500">Cookie Use</span>.
               </div>
             </div>
@@ -61,7 +83,12 @@ const Auth = () => {
             <h3 className="font-medium text-xl mb-4">
               Already have an Account?
             </h3>
-            <Button label={"Sign In"} fullWidth outline />
+            <Button
+              label={"Sign In"}
+              fullWidth
+              outline
+              onClick={onOpenLoginModal}
+            />
           </div>
         </div>
       </div>
